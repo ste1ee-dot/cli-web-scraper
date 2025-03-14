eb510b4b3842261db676b15b8f29f554a340f9f5
Stefan <master2005pop@gmail.com>
2025-03-14 23:10:43 +0100

    Program now shows all unique links in starter page

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module cli_web_scraper
 
-go 1.22.2
+go 1.23.0
+
+toolchain go1.23.7
+
+require golang.org/x/net v0.37.0 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,110 @@
+// TODO   check GET return code for each link
+//		if link is outside domain = skip
+//		if link is inside domain = go to it and check for link
+//		repeat
+
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"os"
+	//	"strings"
+
+	"golang.org/x/net/html"
+)
+
+func getHref(t html.Token) (ok bool, href string) {
+	for _, a := range t.Attr {
+		if a.Key == "href" {
+			href = a.Val
+			ok = true
+		}
+	}
+
+	return
+
+}
+
+func extractLinks(url string, ch chan string, chFinished chan bool) {
+
+	resp, err := http.Get(url)
+
+	defer func() {
+		chFinished <- true
+
+	}()
+
+	if err != nil {
+		fmt.Println("ERROR: Failed to get links:", url)
+		return
+	}
+
+	b := resp.Body
+	defer b.Close()
+
+	z := html.NewTokenizer(b)
+
+	for {
+		tt := z.Next()
+
+		switch {
+		case tt == html.ErrorToken:
+			return
+		case tt == html.StartTagToken:
+			t := z.Token()
+
+			isA := t.Data == "a"
+			if !isA {
+				continue
+			}
+
+			ok, url := getHref(t)
+			if !ok {
+				continue
+			}
+
+			ch <- url
+
+			//	hasHTTP := strings.Index(url, "http") == 0
+			//	if hasHTTP {
+			//		ch <- url
+			//	}
+
+		}
+
+	}
+
+}
 
 func main() {
-	fmt.Println("Something I want to print out")
+
+	//	url := "https://scrape-me.dreamsofcode.io"
+
+	foundUrls := make(map[string]bool)
+	seedUrls := os.Args[1:]
+
+	chUrls := make(chan string)
+	chFinished := make(chan bool)
+
+	for _, url := range seedUrls {
+		go extractLinks(url, chUrls, chFinished)
+	}
+
+	for c := 0; c < len(seedUrls); {
+		select {
+		case url := <-chUrls:
+			foundUrls[url] = true
+		case <-chFinished:
+			c++
+		}
+	}
+
+	fmt.Println("\nFound", len(foundUrls), "unique links:\n")
+
+	for url, _ := range foundUrls {
+		fmt.Println(" - " + url)
+	}
+
+	close(chUrls)
 }
